73c7d237f481465ddedead8e3676efb1a93e60f2
Miguel Arévalo <miguelarevalo1999@gmail.com>
2021-03-20 20:56:57 -0600

    Funciones Vigenere agregadas

--- practica2.py
+++ practica2.py
@@ -53,6 +53,40 @@ def seleccionar_funcion():
             messagebox.showinfo("Error ","You must select an option")
         return n
 
+def generateKey(string, key): 
+    key = list(key) 
+    if len(string) == len(key): 
+        return(key) 
+    else: 
+        for i in range(len(string) - 
+                       len(key)): 
+            key.append(key[i % len(key)]) 
+    return("" . join(key)) 
+
+# This function returns the  
+# encrypted text generated  
+# with the help of the key 
+def cipherText(string, key): 
+    cipher_text = [] 
+    for i in range(len(string)): 
+        x = (ord(string[i]) + 
+             ord(key[i])) % 26
+        x += ord('A') 
+        cipher_text.append(chr(x)) 
+    return("" . join(cipher_text)) 
+      
+# This function decrypts the  
+# encrypted text and returns  
+# the original text 
+def originalText(cipher_text, key): 
+    orig_text = [] 
+    for i in range(len(cipher_text)): 
+        x = (ord(cipher_text[i]) - 
+             ord(key[i]) + 26) % 26
+        x += ord('A') 
+        orig_text.append(chr(x)) 
+    return("" . join(orig_text))
+
 def Euclides(m, n):
 	r=0
 	while (n!=0):
